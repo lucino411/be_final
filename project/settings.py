ece984a4cc625e87292bf64a979a9ae7b11df21c
lucino411 <lucino411@gmail.com>
2024-05-26 11:14:33 -0500

    settings

--- project/settings.py
+++ project/settings.py
@@ -126,8 +126,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL')),
+# }
+
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL')),
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
