5e12486a11582dab433095d007f0522551c7e71d
lucino411 <lucino411@gmail.com>
2024-05-26 13:33:42 -0500

    DEVELOPER MODE

--- project/settings.py
+++ project/settings.py
@@ -2,7 +2,8 @@ from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 import os
 import environ
-import dj_database_url
+import dj_database_url # type: ignore
+import sys
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,9 +131,25 @@ WSGI_APPLICATION = 'project.wsgi.application'
 #     'default': dj_database_url.parse(os.environ.get('DATABASE_URL')),
 # }
 
-DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-}
+# DATABASES = {
+#     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+# }
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
+
+if DEVELOPMENT_MODE is True:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
+elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+    if os.getenv("DATABASE_URL", None) is None:
+        raise Exception("DATABASE_URL environment variable not defined")
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    }
+
 
 
 
